5bcb7c4f86b63ffac89a6b456e3225dba7c766cc
Breison Mendez <breisonmendez78@gmail.com>
2024-05-06 20:23:49 -0500

    Creacion de botones de editar y eliminar

--- src/components/PatientDetails.tsx
+++ src/components/PatientDetails.tsx
@@ -14,7 +14,21 @@ const PatientDetails = ( { patient } : PatientDetailsProps ) => {
         <PatientDetailsItem label="Propietario" data={ patient.caretaker } />
         <PatientDetailsItem label="Fecha" data={ patient.date.toString() } />
         <PatientDetailsItem label="Sintomas" data={ patient.symptoms } />
+        <div className="flex justify-between mt-10">
+          <button
+            type="button"
+            className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg"
+          >
+            Editar
+          </button>
+          <button
+            type="button"
+            className="py-2 px-10 bg-red-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg"
+          >
+            Eliminar
+          </button>
 
+        </div>
     </div>
   )
 }
--- src/components/PatientForm.tsx
+++ src/components/PatientForm.tsx
@@ -6,9 +6,10 @@ import { usePatientStore } from "../store"
 export default function PatientForm() {
   
     const { addPatient } = usePatientStore()
-    const { register , handleSubmit, formState: { errors } } = useForm<DraftPatient>()
+    const { register , handleSubmit, formState: { errors } , reset } = useForm<DraftPatient>()
     const registerPatient = ( data: DraftPatient ) => {
         addPatient(data)
+        reset()
     }
     return (
       <div className="md:w-1/2 lg:w-2/5 mx-5">
